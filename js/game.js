bf8800153bb76269304c19ce08a064d0515349a5
Springbank9 <isabellahay7@gmail.com>
2021-08-05 19:10:14 +0000

    changed acceptingAnswers to false so user can't answer until all is ready and loaded

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion=[];
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
