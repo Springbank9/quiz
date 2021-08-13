0a00323c81bc9ae618523b0e32b05c7fd737bcf7
Springbank9 <isabellahay7@gmail.com>
2021-08-13 18:50:23 +0000

    added extra sections to give a head to the quize such as an area for tracking score and an area for progress. Named variables in js file for question counter and score

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 const progressText = document.getElementById("progressText")
 const hint = document.getElementsByClassName("hint")
 
