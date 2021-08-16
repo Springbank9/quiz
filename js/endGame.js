8e08f5b89106f3117ac7431811c3c546963eeee7
Springbank9 <isabellahay7@gmail.com>
2021-08-16 12:58:18 +0000

    named a variable called most recent score. Created a section to save scores of players. Referenced developer sites to use keyup event and used it for player's username input

--- js/endGame.js
+++ js/endGame.js
@@ -3,6 +3,13 @@ const saveScoreBtn = docuemnt.getElementById("saveScoreBtn");
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
+
+finalScore.innerText = mostRecentScore;
+
 username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
@@ -10,4 +17,19 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
     console.log("clicked the save button!");
     e.preventDefault();
+
+const score = {
+    score: mostRecentScore,
+    name: username.value
+};
+highScores.push(score);
+
+highScores.sort( (a,b) => b.score - a.score)
+
+highScores.splice(5);
+
+localStorage.setItem('highScores', JSON.stringify(highScores));
+window.location.assign("/"); //index.html
+console.log(highScores);
+
 };
