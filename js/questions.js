e78e89374e055a0b593c45f2d64146ba2f6f8a6c
Springbank9 <isabellahay7@gmail.com>
2021-08-17 20:24:48 +0000

    edited entire sections of code as kept running into errors. Brought it back down to basics and re comitting quiz.html and quiz.js

--- js/questions.js
+++ js/questions.js
@@ -4,16 +4,13 @@ let questions = [
         choice1: "<answer1>",
         choice2: "<answer2>",
         choice3: "<answer3>",
-        choice4: "<answer4>",
         answer: 1,
-        hint: "hint"
     }, 
     {
         question: "Question 2?", 
         choice1: "<beetroot>",
         choice2: "<tea>",
         choice3: "<keys>",
-        choice4: "<vape>",
         answer: 2
     }, 
     {
@@ -21,14 +18,12 @@ let questions = [
         choice1: "<answer1>",
         choice2: "<answer2>",
         choice3: "<answer3>",
-        choice4: "<answer4>",
         answer: 3
     }, 
     {   question: "Question 4?", 
         choice1: "<answer1>",
         choice2: "<answer2>",
         choice3: "<answer3>",
-        choice4: "<answer4>",
-        answer: 4
+        answer: 1
 }
 ];
---
+++ js/quiz.js
@@ -0,0 +1,106 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+  {
+      question: "Question 1?", 
+      choice1: "<answer1>",
+      choice2: "<answer2>",
+      choice3: "<answer3>",
+      answer: 1,
+  }, 
+  {
+      question: "Question 2?", 
+      choice1: "<beetroot>",
+      choice2: "<tea>",
+      choice3: "<keys>",
+      answer: 2
+  }, 
+  {
+      question: "Question 3?", 
+      choice1: "<answer1>",
+      choice2: "<answer2>",
+      choice3: "<answer3>",
+      answer: 3
+  }, 
+  {   question: "Question 4?", 
+      choice1: "<answer1>",
+      choice2: "<answer2>",
+      choice3: "<answer3>",
+      answer: 1
+}
+];
+
+//CONSTANTS
+const CORRECT_BONUS = 1;
+const MAX_QUESTIONS = 10;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("/endGame.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
